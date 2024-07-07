7def084c7d0500ebae2d3d3264276a078550dfff
Rakan Alotibi <rakan.w10@outlook.com>
2024-07-08 00:05:36 +0300

    Add: Event Timer

--- Event Countdown/Features/Event/View/Components/EventItem.swift
+++ Event Countdown/Features/Event/View/Components/EventItem.swift
@@ -9,17 +9,28 @@ import SwiftUI
 
 struct EventItem: View {
     let event: Event
+    @State var currentDate: Date = .now
+    private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    private var relativeFormatter = RelativeDateTimeFormatter()
+    
+    init(event: Event) {
+        self.event = event
+    }
+    
     var body: some View {
         VStack (alignment: .leading){
             Text(event.title)
                 .font(.title3)
                 .bold()
                 .foregroundStyle(event.textColor)
-            Text(event.date, formatter: RelativeDateTimeFormatter())
+            Text(relativeFormatter.localizedString(for: event.date, relativeTo: currentDate))
+        }
+        .onReceive(timer){ output in
+            currentDate = output
         }
     }
 }
 
 #Preview {
-    EventItem(event: .init(title: "Title", date: .now + 100, textColor: .red))
+    EventItem(event: .init(title: "Title", date: .now + 13, textColor: .red))
 }
