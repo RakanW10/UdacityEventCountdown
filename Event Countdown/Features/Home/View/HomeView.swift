4a16a77f1dcaaf39a96453a5374cd013afb13687
Rakan Alotibi <rakan.w10@outlook.com>
2024-07-07 20:10:21 +0300

    init commit

---
+++ Event Countdown/Features/Home/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Event Countdown
+//
+//  Created by Rakan Alotibi on 07/07/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
