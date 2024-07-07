c897951ff44d2eaa12b579458ec17b2049e9abee
Rakan Alotibi <rakan.w10@outlook.com>
2024-07-07 16:20:34 +0300

    Initial Commit

---
+++ Event Countdown/Event_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_CountdownApp.swift
+//  Event Countdown
+//
+//  Created by Rakan Alotibi on 07/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Event_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
